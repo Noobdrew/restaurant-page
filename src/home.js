860cb3943be64d53866d7d950fca19c131030776
Ivan Marinski <noobdrew23@gmail.com>
2023-03-02 15:56:56 +0200

    homepage done

--- src/home.js
+++ src/home.js
@@ -1,4 +1,31 @@
-export default function createHome(){
-const title = document.createElement('div')
-title.classList.add('title')
+export default function createHome() {
+
+    const homeElement = document.createElement('div')
+    homeElement.classList.add('home-page')
+
+    const title = document.createElement('div')
+    title.classList.add('title')
+    title.innerText = 'Mercelerri'
+    homeElement.appendChild(title)
+
+    const subTitle = document.createElement('div')
+    subTitle.classList.add('sub-title')
+    subTitle.innerText = 'Le Croissant'
+    title.appendChild(subTitle)
+
+    const titleDisc = document.createElement('div')
+    titleDisc.classList.add('title-description')
+    titleDisc.innerText = 'Come and have one of the best croissants in France, made by the world renowned Gilles Marchal.'
+    homeElement.appendChild(titleDisc)
+
+    const smallDisc = document.createElement('small')
+    smallDisc.innerText = " Venez déguster l'un des meilleurs croissants de France, réalisé par le célèbre Gilles Marchal."
+    titleDisc.appendChild(smallDisc)
+
+    const content = document.querySelector('.content')
+    content.appendChild(homeElement)
+
+    const homePage = content.appendChild(homeElement)
+  
+    return homePage
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
 //only for event listeners and calling modules 
-console.log('yo')
+
+import createHome from './home'
+
+const homeButton = document.querySelector('.home')
+const menuButton = document.querySelector('.menu')
+const contactButton = document.querySelector('.contact')
+const content = document.querySelector('.content')
+
+createHome()
+
+
+homeButton.addEventListener('click', addActive)
+menuButton.addEventListener('click', addActive)
+contactButton.addEventListener('click', addActive)
+
+
+
+function addActive(e) {
+
+    if (e.target.classList.contains('active')) {
+        return 
+    } else {
+        homeButton.classList.remove('active')
+        menuButton.classList.remove('active')
+        contactButton.classList.remove('active')
+        e.target.classList.add('active')
+        console.log(e.target)
+
+        if (homeButton.classList.contains('active')) {
+            content.innerHTML=''
+            console.log('create home')
+            createHome()
+        }
+        if (menuButton.classList.contains('active')) {
+            content.innerHTML=''
+            console.log('create menu')
+        }
+        if (contactButton.classList.contains('active')) {
+            content.innerHTML=''
+            console.log('create contacts')
+        }
+    }
+}
